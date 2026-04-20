1812e107f25ef4d143842ab6161f452cb916e914
Karan Srestha <124446122+KaranSrestha@users.noreply.github.com>
2026-04-20 12:41:38 +0530

    Update server to handle file uploads with multer
    Refactor PDF extraction to use multer for file uploads.

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+const multer = require('multer')
 const pdfParse = require('pdf-parse');
 const express = require('express');
 const cors = require('cors');
 
 const app = express();
+const upload = multer()
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
@@ -10,20 +12,17 @@ app.use(cors());
 
 const port = 3000;
 
-app.post("/extract", async (req, res) => {
+app.post("/extract", 
+    upload.single("file"), async (req, res) => {
     try {
-        let { file1, file2 } = req.body;
+        const buffer = req.file.buffer;
 
-        const buffer1 = Buffer.from(file1.trim(), "base64");
-        const buffer2 = Buffer.from(file2.trim(), "base64");
+        const data = await pdfParse(buffer);
 
-        const data1 = await pdfParse(buffer1);
-        const data2 = await pdfParse(buffer2);
-
-        res.status(200).json({ text1: data1.text, text2: data2.text });
+        res.status(200).json({"text": data.text});
     } catch (e) {
         console.error(e);
-        res.status(500).json({"text1": e.message, "text2": "Error"});
+        res.status(500).json({ "text1":"Error extracting text", "text2": "Error extracting text"});
     }
 });
 
